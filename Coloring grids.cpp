0d9b9344c8b29862a007055126a7f05297ca6f9c
Samarth Tandon <40452187+Samarth9798@users.noreply.github.com>
2019-06-04 10:33:11 +0530

    Create Coloring grids.cpp

---
+++ Coloring grids.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    ll n,k;
+    
+    cin >> n >> k;
+    
+    n == 2 ? cout << k*(k-1)+k : cout << k;
+}
